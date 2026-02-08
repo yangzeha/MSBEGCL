83c99599f0ee7e2691b4a04650e60b414d1fe602
yangzeha <2785602345@qq.com>
2026-02-08 21:29:44 +0800

    feat: Implement Adaptive MSBE-GLSCL with Weighted Fallback mechanism

--- SELFRec/model/graph/MSBEGCL.py
+++ SELFRec/model/graph/MSBEGCL.py
@@ -6,32 +6,83 @@ from util.sampler import next_batch_pairwise
 from base.torch_interface import TorchGraphInterface
 from util.loss_torch import bpr_loss, l2_reg_loss, InfoNCE
 import random
+import numpy as np
 from util.msbe_helper import load_msbe_neighbors
 
 class MSBEGCL(GraphRecommender):
     def __init__(self, conf, training_set, test_set):
         super(MSBEGCL, self).__init__(conf, training_set, test_set)
         args = self.config['MSBEGCL']
-        self.cl_rate = float(args['lambda'])  # Uniformity Weight (SimGCL)
+        self.cl_rate = float(args['lambda'])  # SimGCL Uniformity Weight
         self.msb_rate = float(args['gamma'])   # Similarity Weight (GLSCL/MSBE)
         self.eps = float(args['eps'])
         self.n_layers = int(args['n_layer'])
-        self.temp = float(args.get('tau', 0.2))
+        self.tau = float(args.get('tau', 0.2))
         
-        # Encoder (SimGCL Style)
+        # New: Fallback Weight & Threshold
+        self.fallback_weight = float(args.get('fallback_weight', 0.2))
+        self.sim_threshold = float(args.get('sim_threshold', 0.1))
+        
+        # Encoder
         self.model = MSBEGCL_Encoder(self.data, self.emb_size, self.eps, self.n_layers)
         
         # Load MSB Neighbors
         self.biclique_file = args['biclique.file']
-        self.sim_threshold = float(args.get('sim_threshold', 0.1))
-        
-        self.user_msb_neighbors, self.item_msb_neighbors = load_msbe_neighbors(
+        self.user_msb, self.item_msb = load_msbe_neighbors(
             self.biclique_file,
             self.data.user,
             self.data.item,
             self.data.interaction_mat,
             self.sim_threshold
         )
+
+    def cal_msbe_loss(self, nodes, neighbors_dict, embeddings, is_user=True):
+        """
+        Idea 2: Mean of all members + Weighted Fallback
+        """
+        # 1. Identify which nodes have bicliques
+        has_msbe = []
+        no_msbe = []
+        nodes_np = nodes.cpu().numpy()
+        
+        for idx, node in enumerate(nodes_np):
+            if node in neighbors_dict and len(neighbors_dict[node]) > 0:
+                has_msbe.append(idx)
+            else:
+                no_msbe.append(idx)
+        
+        loss = 0.0
+        # --- Case A: Node has Biclique (Weight = 1.0) ---
+        if len(has_msbe) > 0:
+            target_indices = torch.tensor(has_msbe).cuda()
+            target_nodes = nodes[target_indices]
+            pos_embs = []
+            
+            for node in target_nodes.cpu().numpy():
+                # Mean of all members
+                m_list = neighbors_dict[node]
+                group_emb = torch.mean(embeddings[m_list], dim=0) 
+                pos_embs.append(group_emb)
+            
+            pos_embs = torch.stack(pos_embs)
+            loss += InfoNCE(embeddings[target_nodes], pos_embs, 0.2)
+
+        # --- Case B: No Biclique, Fallback to GLSCL (Weight = fallback_weight) ---
+        if len(no_msbe) > 0:
+            target_indices = torch.tensor(no_msbe).cuda()
+            target_nodes = nodes[target_indices]
+            
+            # Get Top-1 Neighbors (GLSCL logic)
+            top1_embs = self.get_top1_neighbors(target_nodes, embeddings, is_user)
+            loss += self.fallback_weight * InfoNCE(embeddings[target_nodes], top1_embs, 0.2)
+            
+        return loss
+
+    def get_top1_neighbors(self, nodes, all_embeddings, is_user):
+        # Simplified: Random fallback as per instructions.
+        # TODO: Replace with pre-calculated top-1 index lookup.
+        rand_indices = torch.randint(0, len(all_embeddings), (len(nodes),)).cuda()
+        return all_embeddings[rand_indices]
 
     def train(self):
         model = self.model.cuda()
@@ -40,57 +91,47 @@ class MSBEGCL(GraphRecommender):
             for n, batch in enumerate(next_batch_pairwise(self.data, self.batch_size)):
                 user_idx, pos_idx, neg_idx = batch
                 
-                # 1. Main Task: BPR (Clean Embeddings)
-                rec_user_emb, rec_item_emb = model(perturbed=False)
-                user_emb, pos_item_emb, neg_item_emb = rec_user_emb[user_idx], rec_item_emb[pos_idx], rec_item_emb[neg_idx]
+                # 1. SimGCL Global View (Perturbed)
+                u_view1, i_view1 = model(perturbed=True)
+                u_view2, i_view2 = model(perturbed=True)
                 
-                l_main = bpr_loss(user_emb, pos_item_emb, neg_item_emb)
-
-                # 2. L_uniform: SimGCL Noise Contrastive Loss
-                # Generate two independent perturbed views
-                user_v1, item_v1 = model(perturbed=True)
-                user_v2, item_v2 = model(perturbed=True)
+                # 2. Basic Rec View (Clean)
+                rec_user_emb, rec_item_emb = model()
+                # Use only necessary embeddings
+                user_emb = rec_user_emb[user_idx]
+                pos_item_emb = rec_item_emb[pos_idx] 
+                neg_item_emb = rec_item_emb[neg_idx]
                 
+                # --- Loss Calculation ---
+                # (1) Recommendation BPR Loss
+                batch_loss = bpr_loss(user_emb, pos_item_emb, neg_item_emb) + l2_reg_loss(self.reg, user_emb, pos_item_emb, neg_item_emb)
+                
+                # (2) SimGCL Global Contrastive Loss (Uniformity)
                 u_idx_unique = torch.unique(torch.tensor(user_idx).cuda())
                 i_idx_unique = torch.unique(torch.tensor(pos_idx).cuda())
                 
-                l_uniform = InfoNCE(user_v1[u_idx_unique], user_v2[u_idx_unique], self.temp) + \
-                            InfoNCE(item_v1[i_idx_unique], item_v2[i_idx_unique], self.temp)
-
-                # 3. L_sim: Local Similarity Contrastive Loss
-                # Compare Clean Embedding of Node vs Clean Embedding of Neighbor
-                sim_user_idx = self.get_msb_samples(user_idx, self.user_msb_neighbors)
-                sim_item_idx = self.get_msb_samples(pos_idx, self.item_msb_neighbors)
+                cl_loss = self.cl_rate * (InfoNCE(u_view1[u_idx_unique], u_view2[u_idx_unique], self.tau) + 
+                                         InfoNCE(i_view1[i_idx_unique], i_view2[i_idx_unique], self.tau))
                 
-                l_sim = InfoNCE(rec_user_emb[user_idx], rec_user_emb[sim_user_idx], self.temp) + \
-                        InfoNCE(rec_item_emb[pos_idx], rec_item_emb[sim_item_idx], self.temp)
-
-                # 4. Total Loss
-                batch_loss = l_main + \
-                             self.cl_rate * l_uniform + \
-                             self.msb_rate * l_sim + \
-                             l2_reg_loss(self.reg, user_emb, pos_item_emb, neg_item_emb)
+                # (3) MSBE Structural Contrastive Loss (Idea 2)
+                msbe_loss = self.msb_rate * (
+                    self.cal_msbe_loss(u_idx_unique, self.user_msb, rec_user_emb, True) +
+                    self.cal_msbe_loss(i_idx_unique, self.item_msb, rec_item_emb, False)
+                )
+                
+                total_loss = batch_loss + cl_loss + msbe_loss
                 
                 optimizer.zero_grad()
-                batch_loss.backward()
+                total_loss.backward()
                 optimizer.step()
                 
                 if n % 100 == 0 and n > 0:
-                     print('training:', epoch + 1, 'batch', n, 'rec_loss:', l_main.item(), 'cl_uniform:', l_uniform.item(), 'cl_sim:', l_sim.item())
+                     print('training:', epoch + 1, 'batch', n, 'rec_loss:', batch_loss.item(), 'cl_uniform:', cl_loss.item(), 'msbe_struct:', msbe_loss.item())
             
             with torch.no_grad():
                 self.user_emb, self.item_emb = self.model(perturbed=False)
             self.fast_evaluation(epoch)
         self.user_emb, self.item_emb = self.best_user_emb, self.best_item_emb
-
-    def get_msb_samples(self, indices, neighbor_dict):
-        res = []
-        for i in indices:
-            if i in neighbor_dict and len(neighbor_dict[i]) > 0:
-                res.append(random.choice(neighbor_dict[i]))
-            else:
-                res.append(i) # Fallback to Self
-        return torch.tensor(res).cuda()
 
     def save(self):
         with torch.no_grad():
